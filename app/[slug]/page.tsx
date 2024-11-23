a35bef1bbf8b5423706e01a4a6fb28bd4ebe9ccb
Tiago Dias Laureano <tiagodiaslaureano32@gmail.com>
2024-11-22 22:31:02 -0300

    create project

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { motion } from "motion/react"; // Ajuste do import do motion
+import { useParams } from "next/navigation";
+
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  endpoint: "https://s3.filebase.com",
+  accessKeyId: "28BD0B72547222DA700D",
+  secretAccessKey: "MAlJKjEgdFme3yE1GmpYMI5Hsaep1uKWxQXXiWy3",
+  s3ForcePathStyle: true,
+});
+
+export default function QRCode() {
+  const { slug } = useParams(); // pegar o slug params
+  const [imageUrl, setImageUrl] = useState(""); // Estado para guardar o URL da imagem
+
+  useEffect(() => {
+    async function getImgs() {
+      const params = { Bucket: "euteamuuu" };
+
+      try {
+        const response = await s3.listObjectsV2(params).promise();
+        console.log("Arquivos no bucket:", response);
+
+        // Pegando o primeiro objeto do bucket como exemplo
+        if (response.Contents && response.Contents.length > 0) {
+          const fileKey = response.Contents[0].Key;
+          const url = s3.getSignedUrl("getObject", {
+            Bucket: "euteamuuu",
+            Key: fileKey,
+            Expires: 3600, // URL válida por 1 hora
+          });
+          console.log(url);
+          setImageUrl(url); // Salvar o URL no estado
+        }
+      } catch (error) {
+        console.error("Erro ao listar arquivos:", error);
+      }
+    }
+
+    getImgs();
+  }, []);
+
+  return (
+    <div className="bg-gradient-to-b from-[#1E2637] via-[#232A42] to-[#1E2637] min-h-screen text-white">
+      <div className="logo flex justify-center pt-8">
+        <img src="/logo.svg" alt="euteamuuu logo" width={200} />
+      </div>
+
+      {/* Foto + Contadores */}
+      <main className="container max-w-3xl mx-auto mt-10 text-center">
+        <div className="text-[#BF2F32] font-bold text-4xl">Tiago & Raiane</div>
+        <div className="relative mx-auto rounded-lg shadow-lg p-6">
+          {/* Exibir a imagem dinâmica */}
+          {imageUrl && (
+            <img
+              src={imageUrl} // Exibe o placeholder caso o URL ainda não esteja pronto
+              alt="Foto de casal"
+              className="rounded-lg mx-auto shadow-md"
+            />
+          )}
+          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4 mt-6">
+            {/* Card de contadores */}
+            {[
+              { label: "Anos", value: "4" },
+              { label: "Meses", value: "1" },
+              { label: "Dias", value: "25" },
+              { label: "Minutos", value: "25" },
+              { label: "Segundos", value: "25" },
+            ].map((counter, index) => (
+              <motion.div
+                key={index}
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ delay: index * 0.2 }}
+                className="bg-[#1E2637] border shadow-[0px_-2px_8px_1px_#BF2F32] border-[#BF2F32] p-3 rounded-lg "
+              >
+                <h3 className="text-2xl font-bold text-[#BF2F32]">
+                  {counter.value}
+                </h3>
+                <p className="text-sm">{counter.label}</p>
+              </motion.div>
+            ))}
+          </div>
+        </div>
+
+        {/* Mensagem personalizada */}
+        <div className="mt-8">
+          <p className="text-lg italic text-gray-300">
+            "Oi amor, passando pra falar que te amo uma galáxia! Que daqui 20,
+            30 anos possamos voltar aqui e nosso tempo juntos continue sendo
+            contado!"
+          </p>
+          <p className="mt-4 font-bold text-[#BF2F32]">Te amo demais ❤️</p>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="mt-14 text-center">
+        <p className="text-sm text-gray-400">
+          © 2024 euteamuuu. Todos os direitos reservados.
+        </p>
+      </footer>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,96 @@
-import Image from "next/image";
+"use client";
+
+import { useScroll } from "motion/react";
+import { motion } from "motion/react";
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+  const data = [
+    {
+      text: "Plano 1",
+      label: "QRCode, 5 fotos e música.",
+      value: "simples",
+      desc: `Por apenas R$ 19,90, você pode criar uma página exclusiva com até 5 fotos, adicionar uma música especial direto do YouTube e garantir que sua página fique guardada para sempre no nosso site. Tudo isso para eternizar o seu amor de forma única! 💖`,
+    },
+    {
+      text: "Plano 2",
+      label:
+        "QRCode, Galeria de fotos, música, linha do tempo e descontos na lojinha do amor.",
+      value: "avancado",
+      desc: `Eleve a experiência do seu amor por apenas R$ 39,90! Tenha acesso a uma página ilimitada de fotos, adicione sua música preferida do YouTube, crie uma linha do tempo personalizada, adicione lembretes de marcos importantes, inclua um vídeo emocionante de declaração e aproveite muitas outras novidades incríveis que estão por vir. Transforme o ordinário em algo extraordinário! 🌟`,
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="bg-gradient-to-b from-[#1E2637] via-[#232A42] to-[#1E2637] md:h-screen">
+        <motion.div style={{ scaleX: scrollYProgress }} />
+        <div className="container max-w-[1280px] mx-auto px-4">
+          <nav className="pt-8">
+            <div className="logo flex justify-center">
+              <img src="/logo.svg" alt="euteamuuu logo" width={200} />
+            </div>
+          </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <section className="py-14 flex flex-col items-center px-2 md:px-0 text-center">
+            <h1 className="md:text-5xl text-3xl font-bold text-[#BF2F32]">
+              Uma nova maneira de dizer: Eu te amo! 🥰
+            </h1>
+
+            <p className="text-white text-lg max-w-[800px] mt-4">
+              Crie uma página exclusiva para celebrar o amor de vocês!
+              Cadastre-se agora e tenha acesso a uma página personalizada com
+              uma linha do tempo especial e totalmente customizável, um contador
+              que registra os dias, meses e anos juntos, lembretes e
+              notificações de momentos importantes, além de um QR Code para
+              compartilhar essa experiência única com seu parceiro. 💕
+            </p>
+
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="bg-gradient-to-r from-red-500 to-pink-500 px-8 py-4 mt-10 text-white font-bold rounded-md hover:from-red-400 hover:to-pink-400 transition-all"
+            >
+              Crie agora o seu <strong>euteamuuu</strong>!
+            </motion.button>
+          </section>
+
+          <section className="mt-14">
+            <h2 className="text-3xl text-center text-[#BF2F32] font-bold mb-8 ">
+              Planos disponíveis
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              {data.map((plan, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: index * 0.2 }}
+                  className="relative bg-[#232A42] p-6 rounded-lg shadow-[0px_-2px_20px_5px_#BF2F32] border border-[#BF2F32]/20 flex flex-col justify-between h-full"
+                >
+                  {index === 1 && (
+                    <div className="absolute top-0 right-0 bg-[#BF2F32] text-white text-xs font-bold py-1 px-3 rounded-bl-lg">
+                      Mais escolhido
+                    </div>
+                  )}
+
+                  <div>
+                    <h3 className="text-xl text-white font-bold">
+                      {plan.label}
+                    </h3>
+                    <p className="text-white text-sm mt-4">{plan.desc}</p>
+                  </div>
+                  <button
+                    type="button"
+                    className="bg-[#BF2F32] px-4 py-2 mt-6 text-white rounded-md hover:bg-[#BF2F32]/80 shadow-lg"
+                  >
+                    Escolher esse!
+                  </button>
+                </motion.div>
+              ))}
+            </div>
+          </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
